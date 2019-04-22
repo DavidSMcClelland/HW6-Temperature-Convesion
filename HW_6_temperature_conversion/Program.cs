5fc897c35c01c1ad16280e7f67b7e5ff34ee1055
David McClelland <48347439+DavidSMcClelland@users.noreply.github.com>
2019-04-22 15:37:32 -0400

    Add files via upload

---
+++ HW_6_temperature_conversion/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_6_temperature_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int temperature = 0;
+            string end;
+          
+
+            do
+            {
+
+                Console.WriteLine("Please enter a temperature");
+                string reply = Console.ReadLine();
+                bool isNumerical = int.TryParse(reply, out temperature);
+
+                Console.WriteLine("Enter 'C' to convert this to Celsius or enter 'F' to convert this to Fahrenheit?");
+                string tempType = Console.ReadLine();
+
+                        if (tempType == "c")
+                        {
+                            temperature = (temperature - 32) * 5 / 9;
+                            Console.WriteLine("The temperature in Celsius  is: " + temperature);
+                        }
+
+                        else if (tempType == "f")
+                        {
+                            temperature = (temperature * 9 / 5) + 32;
+                            Console.WriteLine("The temperature in Fahrenheit is: " + temperature);
+                        }
+
+                Console.WriteLine("Do you want to check another temperature?");
+                end = Console.ReadLine();
+
+                if (end == "n")
+                { break;
+                }
+            } while (true) ; 
+            
+        }
+
+    }
+}  ///temperature = answer !="Exit"
